dac28d5c1521d1c164b67386a54bf822768c17cf
eggeaterbeater <qwertyweww1122@gmail.com>
2026-07-15 14:19:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-yesyysy
+const display = document.getElementById("display");
+
+function appendValue(value){
+
+display.value += value;
+
+}
+
+function clearDisplay(){
+
+display.value="";
+
+}
+
+function calculate(){
+
+try{
+
+display.value = eval(display.value);
+
+display.classList.add("success");
+
+setTimeout(()=>{
+
+display.classList.remove("success");
+
+},400);
+
+}
+
+catch{
+
+display.value="Error";
+
+display.classList.add("shake");
+
+setTimeout(()=>{
+
+display.classList.remove("shake");
+
+},400);
+
+}
+
+}
+
+function percentage(){
+
+display.value = eval(display.value)/100;
+
+}
+
+function squareRoot(){
+
+display.value = Math.sqrt(eval(display.value));
+
+}
+
+function power(){
+
+display.value = Math.pow(eval(display.value),2);
+
+}
